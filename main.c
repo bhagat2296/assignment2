25ead968d71bfd032b493b4e45e5e069c081de3e
bhagat2296 <103653090+bhagat2296@users.noreply.github.com>
2022-07-28 12:00:35 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*int x;
+    printf("enter number\n"); //Question 1
+    scanf("%d",&x);
+    printf("unit digit of number is %d",x%10);*/
+
+    /*int x;
+    printf("enter number\n"); //Question 2
+    scanf("%d",&x);
+    printf("unit digit of number is %d",x/10);*/
+
+    /*int temp=0,x,y;
+    printf("enter first number\n"); //Question 3
+    scanf("%d",&x);
+    printf("enter second number");
+    scanf("%d",&y);
+    temp=x;
+    x=y;
+    y=temp;
+
+    printf("value of first number and second number is %d and %d respectively",x,y);*/
+
+    /*int x,y;
+    printf("enter first number\n"); //Question 4
+    scanf("%d",&x);
+    printf("enter second number");
+    scanf("%d",&y);
+
+    x=x+y;
+    y=x-y;
+    x=x-y;
+    printf("value of first number and second number is %d and %d respectively",x,y);*/
+
+    /*int sum=0,x,f=0,m=0,l=0,t=0; //Question 5
+    printf("enter 3 digit number\n");
+    scanf("%d",&x);
+    f=x/100;
+    l=x%10;
+    t=x/10;
+    m=t%10;
+    sum=f+m+l;
+
+    printf("sum is %d",sum);*/
+
+    /*int x;
+    printf("enter a character\n"); //Question 6
+    scanf("%c",&x);
+    printf("%d",x);*/
+
+
+    /*int x;
+    printf("enter a character\n"); //Question 8
+    scanf("%c",&x);
+    if(x&1)
+        printf("nmber is odd");
+
+    else
+        printf("number is even");*/
+
+
+
+    /*int i; //Question 9
+    char c;
+    float f;
+    double d;
+    long l;
+
+    printf("size of an integer is %d Byte\n",sizeof(i));
+    printf("size of character is %d Byte\n",sizeof(c));
+    printf("size of float variable is %d Byte\n",sizeof(f));
+    printf("size of double is %d Byte\n",sizeof(d));
+    printf("size of long is %d Byte\n",sizeof(l));*/
+
+
+    /*float x;  //Question 12
+    //given 1 USD=76.23 rupees
+
+    printf("enter amount in INR\n");
+    scanf("%f",&x);
+
+    printf("Entered Amount(Rupees) is %f USD",x/76.23);*/
+
+
+   //QUESTIONS 7/10/11/13 HAVE NOT SOLVED
+
+
+
+    return 0;
+}
